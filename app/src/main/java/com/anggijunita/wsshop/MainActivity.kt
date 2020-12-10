b68a8b472159d28b6037f2164161a675378a607e
anggijunita <71134019+anggijunita@users.noreply.github.com>
2020-12-10 19:16:22 +0700

    instagram

---
+++ app/src/main/java/com/anggijunita/wsshop/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.gunawan.wsshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.title = "Laptop ROG"
+        }
+    }
+}
